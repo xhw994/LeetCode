64fbdedcf65911b62014f348ac9d8ca2a4b5cc2e
xwa132 <xwa132@sfu.ca>
2019-03-19 23:12:27 -0700

    965

---
+++ LeetCode/LeetCode951_1000/965_Univalued_Binary_Tree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Common;
+
+namespace LeetCode.LeetCode
+{
+    public static class LeetCode951_1000
+    {
+        public static bool IsUnivalTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            int x = root.val;
+            bool res = true;
+            Traverse(root);
+            return res;
+
+            void Traverse(TreeNode node)
+            {
+                if (node.val != x)
+                {
+                    res = false;
+                }
+                if (node.left != null)
+                {
+                    Traverse(node.left);
+                }
+                if (node.right != null)
+                {
+                    Traverse(node.right);
+                }
+            }
+        }
+    }
+}
