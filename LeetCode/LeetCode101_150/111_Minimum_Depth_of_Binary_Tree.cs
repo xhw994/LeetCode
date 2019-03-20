2594dc642fa106a9456ae8f3536c9c1d33b542a8
xwa132 <xwa132@sfu.ca>
2019-03-20 00:38:31 -0700

    111

--- LeetCode/LeetCode101_150/111_Minimum_Depth_of_Binary_Tree.cs
+++ LeetCode/LeetCode101_150/111_Minimum_Depth_of_Binary_Tree.cs
@@ -11,30 +11,19 @@ namespace LeetCode
     {
         public static int MinDepth(TreeNode root)
         {
-            int min = int.MaxValue;
-
             if (root == null)
             {
                 return 0;
             }
-            if (root.left == null && root.right == null)
-            {
-                return 1;
-            }
-            if (root.left != null)
-            {
-                Helper(root.left, 1);
-            }
-            if (root.right != null)
-            {
-                Helper(root.right, 1);
-            }
+
+            int min = int.MaxValue;
+            Helper(root, 1);
             
             return min;
 
             void Helper(TreeNode node, int dep)
             {
-                if (node == null)
+                if (node.left == null && node.right == null)
                 {
                     if (dep < min)
                     {
@@ -42,8 +31,15 @@ namespace LeetCode
                     }
                     return;
                 }
-                Helper(node.left, dep + 1);
-                Helper(node.right, dep + 1);
+
+                if (node.left != null)
+                {
+                    Helper(node.left, dep + 1);
+                }
+                if (node.right != null)
+                {
+                    Helper(node.right, dep + 1);
+                }
             }
         }
     }
