f8a060fbc638f51f550963e16e011e9f7a105fb3
xwa132 <xwa132@sfu.ca>
2019-03-21 21:50:59 -0700

    747

---
+++ LeetCode/LeetCode701_750/747_Largest_Number_At_Least_Twice_of_Others.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public static partial class LeetCode701_750
+    {
+        public static int DominantIndex(params int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+            int mi = 0, sv = int.MinValue;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[mi])
+                {
+                    sv = nums[mi];
+                    mi = i;
+                }
+                else if (nums[i] > sv)
+                {
+                    sv = nums[i];
+                }
+            }
+            return nums[mi] - sv * 2 >= 0 ? mi : -1;
+        }
+    }
+}
