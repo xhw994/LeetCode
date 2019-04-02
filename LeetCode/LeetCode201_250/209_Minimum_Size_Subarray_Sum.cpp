191d93bc26784b479c421e926720064e339e1886
xwa132 <xwa132@sfu.ca>
2019-04-02 02:35:47 -0700

    209++

---
+++ LeetCode/LeetCode201_250/209_Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+	int minSubArrayLen(int s, vector<int>& nums) {
+		if (nums.empty()) {
+			return 0;
+		}
+		
+		unsigned int l = 0, sum = 0, o = UINT_MAX;
+		for (unsigned int r = 0; r < nums.size(); ++r) {
+			sum += nums[r];
+			while (sum >= s) {
+				o = min(o, r - l + 1);
+				sum -= nums[l++];
+			}
+		}
+		return o == UINT_MAX ? 0 : o;
+	}
+};
