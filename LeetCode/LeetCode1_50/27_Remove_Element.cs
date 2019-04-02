508ca46fc798d2f5bba8e31fd372a9f325ce47ba
xwa132 <xwa132@sfu.ca>
2019-04-01 19:12:35 -0700

    27++

--- LeetCode/LeetCode1_50/27_Remove_Element.cs
+++ LeetCode/LeetCode1_50/27_Remove_Element.cs
@@ -23,3 +23,20 @@ namespace LeetCode
         }
     }
 }
+
+/*
+C++ Solution:
+int removeElement(vector<int>& nums, int val) {
+		if (nums.empty()) {
+			return 0;
+		}
+		int j = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			if (nums[i] != val) {
+				nums[j++] = nums[i];
+			}
+		}
+		return j;
+	}
+
+ */
