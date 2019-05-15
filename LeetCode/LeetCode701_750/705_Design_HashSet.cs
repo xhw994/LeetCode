a44659cf6526ec8e58fc93b350002db1125910f6
xwa132 <xwa132@sfu.ca>
2019-05-15 03:20:54 -0700

    705 better cheat

---
+++ LeetCode/LeetCode701_750/705_Design_HashSet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public static partial class LeetCode701_750
+    {
+        public class MyHashSet
+        {
+            private bool[] mark;
+            /** Initialize your data structure here. */
+            public MyHashSet()
+            {
+                mark = new bool[1000001];
+            }
+
+            public void Add(int key)
+            {
+                mark[key] = true;
+            }
+
+            public void Remove(int key)
+            {
+                mark[key] = false;
+            }
+
+            /** Returns true if this set contains the specified element */
+            public bool Contains(int key)
+            {
+                return mark[key];
+            }
+        }
+
+        /**
+         * Your MyHashSet object will be instantiated and called as such:
+         * MyHashSet obj = new MyHashSet();
+         * obj.Add(key);
+         * obj.Remove(key);
+         * bool param_3 = obj.Contains(key);
+         */
+    }
+}
