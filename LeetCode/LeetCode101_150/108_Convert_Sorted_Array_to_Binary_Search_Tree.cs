9957ed7e66fb36f185b9b5dd16f5c189be9bc584
xwa132 <xwa132@sfu.ca>
2019-05-16 02:14:55 -0700

    108

---
+++ LeetCode/LeetCode101_150/108_Convert_Sorted_Array_to_Binary_Search_Tree.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public static partial class LeetCode101_150
+    {
+        public static TreeNode SortedArrayToBST(int[] nums)
+        {
+            return BstHelper(0, nums.Length - 1);
+    
+            TreeNode BstHelper(int left, int right)
+            {
+                if (left > right) return null;
+                if (left == right) return new TreeNode(nums[left]);
+
+                int mid = left + (right - left) / 2;
+                return new TreeNode(nums[mid])
+                {
+                    left = BstHelper(left, mid - 1),
+                    right = BstHelper(mid + 1, right)
+                };
+            }
+        }
+    }
+}
