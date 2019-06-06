611b4484db37a7342e839333498c3376f901ca6d
xhw994 <wxh960302@gmail.com>
2019-06-05 21:22:00 -0700

    51_100

--- LeetCode/LeetCode51_100/54_Spiral_Matrix.cpp
+++ LeetCode/LeetCode51_100/54_Spiral_Matrix.cpp
@@ -3,43 +3,39 @@
 #include <algorithm>
 #include <climits>
 #include <string>
+#include "../..//LeetCodeCpp/LeetCode51_100.h"
 
 using namespace std;
 
-namespace LeetCode {
-    class LeetCode51_100 {
-    public:
-        vector<int> spiralOrder(vector<vector<int>>& matrix) {
-            if (matrix.empty()) {
-                return {};
-            }
+vector<int> LeetCode::LeetCode51_100::spiralOrder(vector<vector<int>>& matrix) {
+    if (matrix.empty()) {
+        return {};
+    }
 
-            vector<int> res(matrix.size() * matrix[0].size());
-            int r1 = 0, r2 = matrix.size() - 1;
-            int c1 = 0, c2 = matrix[0].size() - 1;
-            int k = 0;
-            while (r1 <= r2 && c1 <= c2) {
-                for (int i = c1; i <= c2; ++i) {
-                    res[k++] = matrix[r1][i];
-                }
-                r1++;
-                for (int i = r1; i <= r2; ++i) {
-                    res[k++] = matrix[i][c2];
-                }
-                c2--;
-                if (r1 <= r2 && c1 <= c2) {
-                    for (int i = c2; i >= c1; --i) {
-                        res[k++] = matrix[r2][i];
-                    }
-                    r2--;
-                    for (int i = r2; i >= r1; --i) {
-                        res[k++] = matrix[i][c1];
-                    }
-                    c1++;
-                }
-            }
-
-            return res;
+    vector<int> res(matrix.size() * matrix[0].size());
+    int r1 = 0, r2 = matrix.size() - 1;
+    int c1 = 0, c2 = matrix[0].size() - 1;
+    int k = 0;
+    while (r1 <= r2 && c1 <= c2) {
+        for (int i = c1; i <= c2; ++i) {
+            res[k++] = matrix[r1][i];
         }
-    };
+        r1++;
+        for (int i = r1; i <= r2; ++i) {
+            res[k++] = matrix[i][c2];
+        }
+        c2--;
+        if (r1 <= r2 && c1 <= c2) {
+            for (int i = c2; i >= c1; --i) {
+                res[k++] = matrix[r2][i];
+            }
+            r2--;
+            for (int i = r2; i >= r1; --i) {
+                res[k++] = matrix[i][c1];
+            }
+            c1++;
+        }
+    }
+
+    return res;
 }
---
+++ LeetCodeCpp/LeetCode51_100.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef _LEETCODE_51100
+#define _LEETCODE_51100
+#include <vector>
+#include <string>
+
+namespace LeetCode {
+    class LeetCode51_100 {
+    public:
+        static std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix);
+    };
+}
+#endif // !_LEETCODE_51100
