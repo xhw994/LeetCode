a49a2e8a15e152c7a550ebbdfdadea9c98f3f0bd
xhw994 <wxh960302@gmail.com>
2019-08-06 16:39:53 -0700

    303++

--- LeetCodeCpp/Common.h
+++ LeetCodeCpp/Common.h
@@ -11,3 +11,14 @@ struct TreeNode {
 };
 
 #endif // !_LEETCODE_TREENODE
+
+#ifndef _LEETCODE_FASTIO
+#define _LEETCODE_FASTIO
+
+static const int fastIO = []() {  //读入外挂 std::ios::sync_with_stdio(false); 加快读入速度
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+#endif
--- LeetCodeCpp/LeetCode.h
+++ LeetCodeCpp/LeetCode.h
@@ -4,5 +4,6 @@
 #include "LeetCode101_150.h"
 #include "LeetCode151_200.h"
 #include "LeetCode201_250.h"
+#include "LeetCode301_350.h"
 #include "LeetCode451_500.h"
 #include "LeetCode551_600.h"
